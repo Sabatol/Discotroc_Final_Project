cb541cf3244e6139a6b30389429f51368339bf05
ChriZoizo <christeufeur83@gmail.com>
2020-11-30 18:23:07 +0100

    Update User Table, Create User Library, Disc State and relation between updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_in) do |user_params|
-      user_params.permit(:password, :email)
+      user_params.permit(:password, :email, :first_name, :last_name, :address, :zipcode, :city, :country)
     end
   end
 end
