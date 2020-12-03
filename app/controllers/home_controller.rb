31406eca138881fdd573a41e951d103a4e85d0a0
piwiit <bouillaux@hotmail.fr>
2020-12-03 17:20:09 +0100

    tracklist

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,10 @@ class HomeController < ApplicationController
     @auth_wrapper =
       Discogs::Wrapper.new('Discotroc', user_token: ENV['USER_TOKEN'])
     @wrapper = Discogs::Wrapper.new('Discotroc')
+    puts '*' * 60
     @search = @auth_wrapper.search(params[:search], type: :album)
-    puts @search
+    @release = @wrapper.get_release(@search.id)
+    puts '*' * 60
+    puts @search.id
   end # lines in order to get access to Discogs API and do research directly on Discogs via our app. First it refers to the gem 'discogs-wrapper' then it sets the global variable '$wrapper' used throughout the app to get data from Discogs thanks to our user_token
 end
