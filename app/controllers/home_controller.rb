81e18c5fa6852cd0dbd47b9e7152175e91d7e1dc
piwiit <bouillaux@hotmail.fr>
2020-12-03 14:45:28 +0100

    add APi and seach bar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,10 @@
 class HomeController < ApplicationController
-  require "discogs"
+  require 'discogs'
   def index
-    # lines in order to get access to Discogs API and do research directly on Discogs via our app. First it refers to the gem 'discogs-wrapper' then it sets the global variable '$wrapper' used throughout the app to get data from Discogs thanks to our user_token
-    @auth_wrapper = Discogs::Wrapper.new("Discotroc", user_token: ENV['USER_TOKEN'] )
-    @wrapper = Discogs::Wrapper.new("Discotroc")
-  end
+    @auth_wrapper =
+      Discogs::Wrapper.new('Discotroc', user_token: ENV['USER_TOKEN'])
+    @wrapper = Discogs::Wrapper.new('Discotroc')
+    @search = @auth_wrapper.search(params[:search], type: :album)
+    puts @search
+  end # lines in order to get access to Discogs API and do research directly on Discogs via our app. First it refers to the gem 'discogs-wrapper' then it sets the global variable '$wrapper' used throughout the app to get data from Discogs thanks to our user_token
 end
