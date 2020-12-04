ead94d47b4c6a1698010808a5c3337a86228fbf4
Sabatol <u.sacrez@gmail.com>
2020-12-04 14:17:40 +0100

    update mail in initializer/devise and app/mailer/application_mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'machin@yopmail.com'
+  default from: 'discotroc@yopmail.com'
   layout 'mailer'
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.perform_deliveries = true
   # Uncomment the following line to use letter_opener to deliver mail in development
-  config.action_mailer.delivery_method = :letter_opener
+  # config.action_mailer.delivery_method = :letter_opener
   
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
