db055ed51203c2f14852599a319fd5dca1b56167
ChriZoizo <christeufeur83@gmail.com>
2020-12-04 10:01:57 +0100

    release fix dealpms in deal view

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,9 @@
 module HomeHelper
+    def search_sender_disc_home
+        @deal = Deal.all
+        deal_content = DealContent.where(deal_id: @deal.ids)
+        user = UserLibrary.where(id: deal_content.pluck(:sender_library_id))
+        disc = Disc.where(id: user.pluck(:disc_id))
+        return disc.sample(4)
+        end 
 end
