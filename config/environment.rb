4e97ec0bcaf8e677bc5f54a952fdafbb49892db2
piwiit <bouillaux@hotmail.fr>
2020-12-04 16:34:04 +0100

    update

--- config/environment.rb
+++ config/environment.rb
@@ -6,11 +6,11 @@ Rails.application.initialize!
 
 # Set the SMTP with SendGrid infos
 ActionMailer::Base.smtp_settings = {
-  :user_name => ENV['SENDGRID_LOGIN'],
-  :password => ENV['SENDGRID_PWD'],
-  :domain => 'discotroc-dev.herokuapp.com/',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
+  user_name: ENV['SENDGRID_LOGIN'],
+  password: ENV['SENDGRID_PWD'],
+  domain: 'http://localhost:3000/',
+  address: 'smtp.sendgrid.net',
+  port: 587,
+  authentication: :plain,
+  enable_starttls_auto: true
 }
