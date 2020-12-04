ff3d4b585c49c075724484bde800f5656bf30288
piwiit <bouillaux@hotmail.fr>
2020-12-04 18:25:38 +0100

    add mailler to deal

--- app/mailers/deal_mailer.rb
+++ app/mailers/deal_mailer.rb
@@ -1,9 +1,15 @@
 class DealMailer < ApplicationMailer
-  def send_message_new_deal
-    @sender = User.find(@new_deal.sender_id)
+  def send_message_new_deal_receiver(deal)
+    @new_deal = deal
     @receiver = User.find(@new_deal.receiver_id)
-
     @url = "https://discotroc-dev.herokuapp.com/deals/#{@new_deal.id}"
-    mail(to: @sender.email,@receiver.email, subject: 'Vous vous êtes inscrit sur Discotroc !')
+    mail(to: @receiver.email, subject: 'tu as recu un deal !')
+  end
+
+  def send_message_new_deal_sender(deal)
+    @new_deal = deal
+    @sender = User.find(@new_deal.sender_id)
+    @url = "https://discotroc-dev.herokuapp.com/deals/#{@new_deal.id}"
+    mail(to: @sender.email, subject: 'tu as créé un deal !')
   end
 end
