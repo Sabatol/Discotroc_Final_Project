00facc710b2a54c3826137a55a68d30ae6af6ffb
kill84 <killian.kos@gmail.com>
2020-12-07 17:47:19 +0100

    build articles controller and routes

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,41 @@
+class ArticlesController < ApplicationController
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(title: params[:title], content: params[:content], user: curren_user)
+    if @article.save
+      redirect_to articles_path
+      flash[:notice_good] = "Votre article a bien été publié"
+    else
+      flash[:notice_bad] = "Votre article n'a pas pu être publié"
+      render 'new'
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    post_params = params.require(:article).permit(:title, :content)
+    @article.update(post_params)
+    redirect_to articles_path
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def index
+    @articles = Article.all
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+end
