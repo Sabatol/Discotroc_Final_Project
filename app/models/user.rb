9d32822176e22e12671e8d95cf7e7c0cf060b503
Sabatol <u.sacrez@gmail.com>
2020-12-07 10:14:49 +0100

    Build migration and model with validates for Articles

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,15 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
-  has_many :user_libraries
+  has_many :user_libraries, dependent: :destroy
   has_many :discs, through: :user_libraries
 
   has_many :deals_sender, class_name: 'Deal', foreign_key: 'sender_id'
   has_many :deals_receiver, class_name: 'Deal', foreign_key: 'receiver_id'
 
   has_many :pm_authors, class_name: 'DealPm', foreign_key: 'pm_author_id'
+
+  has_many :articles
 
   devise :database_authenticatable,
          :registerable,
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_01_123239) do
+ActiveRecord::Schema.define(version: 2020_12_07_083446) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
+  end
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
