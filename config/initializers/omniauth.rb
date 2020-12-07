142115d585b61d507d3f0824f8300bcdb444ae5a
kill84 <killian.kos@gmail.com>
2020-12-07 11:12:16 +0100

    finish up Spotify API initial configuration

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 require 'rspotify/oauth'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :spotify, ENV['CLIENT_ID'], ENV['CLIENT_SECRET'], scope: 'user-read-email playlist-modify-public user-library-read user-library-modify'
+  provider :spotify, ENV['CLIENT_ID'], ENV['CLIENT_SECRET'], scope: 'ugc-image-upload user-read-recently-played user-top-read user-read-playback-position user-read-playback-state user-modify-playback-state user-read-currently-playing app-remote-control streaming playlist-modify-public playlist-modify-private playlist-read-private playlist-read-collaborative user-follow-modify user-follow-read user-library-modify user-library-read user-read-email user-read-private'
 end
