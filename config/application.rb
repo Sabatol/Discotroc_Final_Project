04dd2ace4bbc3fa1cd58050b51832f443e509ee2
kill84 <killian.kos@gmail.com>
2020-12-07 14:46:32 +0100

    Spotify API integration

--- config/application.rb
+++ config/application.rb
@@ -1,13 +1,20 @@
 require_relative 'boot'
 
 require 'rails/all'
-require 'rspotify'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-
+# Rails.application.config.middleware.insert_before 0, Racks::Cors do
+#   allow do
+#     origins '*'
+#
+#     resource '*',
+#     headers: :any,
+#     methods: [:get, :post, :put, :patch, :delete, :options, :head]
+#   end
+# end
 
 module Discotroc
   class Application < Rails::Application
