4d3526c3f660a9509fd37c04c9340bea99734b22
piwiit <bouillaux@hotmail.fr>
2020-12-07 12:06:00 +0100

    update model deal and make seed playlist and articles" "

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -2,9 +2,9 @@ class Deal < ApplicationRecord
   belongs_to :sender, class_name: 'User', foreign_key: 'sender_id'
   belongs_to :receiver, class_name: 'User', foreign_key: 'receiver_id'
 
-  has_many :deal_contents
+  has_many :deal_contents, dependent: :destroy
+  has_many :deal_pms, dependent: :destroy
 
-  has_many :deal_pms
   after_create :send_deal_email
   def send_deal_email
     DealMailer.send_message_new_deal_receiver(self).deliver
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_07_083446) do
+ActiveRecord::Schema.define(version: 2020_12_07_091953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -122,6 +122,14 @@ ActiveRecord::Schema.define(version: 2020_12_07_083446) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "playlists", force: :cascade do |t|
+    t.string "title"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_playlists_on_user_id"
   end
 
   create_table "styles", force: :cascade do |t|
