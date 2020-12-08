a4136f37ffb44a57a5444c9d603dac43e5045a4b
Sabatol <u.sacrez@gmail.com>
2020-12-08 10:04:27 +0100

    build controller for comments, update of deals/edit, deals/show and users/show

--- config/routes.rb
+++ config/routes.rb
@@ -3,18 +3,20 @@ Rails.application.routes.draw do
   root 'home#index'
   devise_for :users
 
-  resources :users
+  resources :deals do 
+    resources :comments, only: [:new, :create, :destroy]
+  end
 
+
+  resources :users
   resources :discs
   resources :genres
   resources :tracks
   resources :artists
   resources :styles
-  resources :deals 
   resources :deal_contents
   resources :deal_pms
   resources :user_libraries
   resources :disc_states
 
- 
 end
