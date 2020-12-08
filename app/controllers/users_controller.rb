98226dbe60aff1a47049da3aff231c0420fe8380
Sabatol <u.sacrez@gmail.com>
2020-12-08 15:59:18 +0100

    Update user controller + edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,13 @@ class UsersController < ApplicationController
         @edit_user = User.find(params[:id])
         post_params = params.require(:user).permit(:first_name, :last_name, :address, :additional_address, :city, :zipcode, :country, :description)
         @edit_user.update(post_params)
-        redirect_to user_path(@edit_user.id)
+        if @edit_user.country && @edit_user.first_name && @edit_user.last_name && @edit_user.address && @edit_user.city && @edit_user.zipcode && @edit_user.description  
+            @edit_user.update_attributes(completed: true)
+            redirect_to user_path(@edit_user.id)
+        else
+            @edit_user.update_attributes(completed: false)
+            redirect_to root_path
+        end
     end 
 
     def destroy
