32d5c0c4c84aa4f9ac3109d49373f47011cd0560
piwiit <bouillaux@hotmail.fr>
2020-12-09 16:45:15 +0100

    checkbox show deal

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -1,28 +1,28 @@
 class DealsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
-  before_action :is_completed?, except: [:index, :show]
-  
+  before_action :authenticate_user!, except: %i[index show]
+  before_action :is_completed?, except: %i[index show]
+
   def index
     @deals = Deal.all
-  end 
+  end
 
   def show
     @deal = Deal.find(params[:id])
-  end 
+  end
 
   def new
     @sender = User.find(params[:sender_id])
-    @new_deal = Deal.new 
-  end 
+    @new_deal = Deal.new
+  end
 
   def create
     @sender = User.find(params[:sender_id])
     @new_deal = Deal.new(sender_id: @sender.id, receiver_id: current_user.id)
     if @new_deal.save
-      flash[:notice_good] = "Le deal a bien été créer"
+      flash[:notice_good] = 'Le deal a bien été créer'
       redirect_to deal_path(@new_deal.id)
     else
-      flash[:notice_bad] = "Le disque a bien été créer"
+      flash[:notice_bad] = 'Le disque a bien été créer'
       render 'new'
     end
   end
@@ -33,7 +33,13 @@ class DealsController < ApplicationController
 
   def update
     @edit_deal = Deal.find(params[:id])
-    post_params = params.require(:deal).permit(:sender_id, :receiver_id, :sender_ok, :receiver_ok)
+    post_params =
+      params.require(:deal).permit(
+        :sender_id,
+        :receiver_id,
+        :sender_ok,
+        :receiver_ok
+      )
     @edit_deal.update(post_params)
     redirect_to deal_path(@edit_deal)
   end
@@ -42,5 +48,5 @@ class DealsController < ApplicationController
     @destroy_deal = Deal.find(params[:id])
     @destroy_deal.destroy
     redirect_to deals_path
-  end 
+  end
 end
