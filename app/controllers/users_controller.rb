36ef36cf711a8e6a0df48af3167aa98f685b82ed
kill84 <killian.kos@gmail.com>
2020-12-09 23:29:34 +0100

    ActiveStorage configuration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,19 +2,19 @@ class UsersController < ApplicationController
     before_action :authenticate_user!, except: [:index, :new]
     def index
         @users = User.all
-    end 
+    end
 
-    def show 
+    def show
         @user = User.find(params[:id])
-    end 
+    end
 
     def edit
         @edit_user = User.find(params[:id])
-    end 
+    end
 
     def update
         @edit_user = User.find(params[:id])
-        post_params = params.require(:user).permit(:first_name, :last_name, :address, :additional_address, :city, :zipcode, :country, :description)
+        post_params = params.require(:user).permit(:first_name, :last_name, :address, :additional_address, :city, :zipcode, :country, :description, :avatar)
         @edit_user.update(post_params)
         if user_completed?(@edit_user)
             @edit_user.update_attributes(completed: true)
@@ -28,20 +28,20 @@ class UsersController < ApplicationController
             @edit_user.update_attributes(completed: false)
             redirect_to root_path
         end
-    end 
+    end
 
     def destroy
         @destroy_user = User.find(params[:id])
         @destroy_user.destroy
-        redirect_to users_path     
+        redirect_to users_path
     end
 
 
     private
     def user_completed?(user)
-        if user.country.present? && user.first_name.present? && user.last_name.present? && user.address.present? && user.city.present? && user.zipcode.present? && user.description.present? 
+        if user.country.present? && user.first_name.present? && user.last_name.present? && user.address.present? && user.city.present? && user.zipcode.present? && user.description.present?
             true
-        else 
+        else
             false
         end
     end
