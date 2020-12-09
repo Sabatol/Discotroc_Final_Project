27f5f75be43512593d642f36069a2b265759118d
piwiit <bouillaux@hotmail.fr>
2020-12-09 10:23:15 +0100

    alert confirm user library

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,11 +1,8 @@
-
+//= require jquery
+//= require jquery_ujs
 //= require jquery-3.2.1.min
 //= require bootstrap.min
 //= require jquery.slicknav.min
 //= require owl.carousel.min
 //= require mixitup.min
 //= require main
-
-
-
-
