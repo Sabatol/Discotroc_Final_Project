afc68c19bcf8a43e7e7cea3eada6c182af123e18
Sabatol <u.sacrez@gmail.com>
2020-12-10 11:30:23 +0100

    Merge main in branch

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,12 @@ Rails.application.routes.draw do
 
   resources :users do
     resources :user_libraries, except: [:index, :show]
+    resources :avatars, only: [:create]
   end
 
 
   resources :user_libraries, only: [:index, :show] do
-    resources :deals, except: [:index] do 
+    resources :deals, except: [:index] do
       resources :comments, only: [:new, :create, :destroy]
       resources :deal_contents
       resources :deal_pms
