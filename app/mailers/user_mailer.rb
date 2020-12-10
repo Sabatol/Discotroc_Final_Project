e92198e9ae45f2d72a09e5a1a302309bc08d2571
piwiit <bouillaux@hotmail.fr>
2020-12-10 23:53:01 +0100

    custom routes path

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ApplicationMailer
   def send_message_new_user(user)
     @user = user
-    @url = "google.com"
-    mail(:to => @user.email, :subject => "Vous vous êtes inscrit sur Discotroc !")
+    @url = 'https://discotroc-dev.herokuapp.com'
+    mail(to: @user.email, subject: 'Vous vous êtes inscrit sur Discotroc !')
   end
 end
