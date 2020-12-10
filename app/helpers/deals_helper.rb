904ad77670fea2da8f42f49879460257ac002ddd
kill84 <killian.kos@gmail.com>
2020-12-10 12:39:27 +0100

    secure deal views

--- app/helpers/deals_helper.rb
+++ app/helpers/deals_helper.rb
@@ -3,5 +3,13 @@ module DealsHelper
     @deal = Deal.find(params[:id])
     @deal_pm = DealPm.where(deal_id: @deal.id)
     return @deal_pm
-    end 
+    end
+
+    def is_deal_stakeholder
+        deal = Deal.find(params[:id])
+        if current_user != deal.sender && current_user != deal.receiver &&  current_user.is_admin? == false
+          redirect_to root_path
+          flash[:deal_alert] = "Vous ne participez pas à ce troc"
+        end
+    end
 end
