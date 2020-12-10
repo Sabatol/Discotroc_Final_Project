33a1c177a89498a6c851d857a5c865e7faa08a5b
piwiit <bouillaux@hotmail.fr>
2020-12-11 00:11:25 +0100

    alias routes/fix bug user-lybrary/ made partiel user library

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,25 @@
 Rails.application.routes.draw do
   root 'home#index'
-  devise_for :users, path: 'Utilisateur'
+  devise_for :users, path: 'utilisateur'
 
-  resources :users, path: 'Utilisateur' do
-    resources :user_libraries, except: %i[index show], path: 'Bibliothèques'
+  resources :users, path: 'utilisateur' do
+    resources :user_libraries, except: %i[index show], path: 'bibliothèques'
 
     resources :avatars, only: %i[create]
   end
 
-  resources :user_libraries, only: %i[index show], path: 'Bibliothèques' do
+  resources :user_libraries, only: %i[index show], path: 'bibliothèques' do
     resources :deals, except: %i[index], path: 'Echange' do
-      resources :comments, only: %i[new create destroy], path: 'Bibliothèques'
-      resources :deal_contents, path: 'Proposition'
-      resources :deal_pms, path: 'Message-privé'
+      resources :comments, only: %i[new create destroy], path: 'bibliothèques'
+      resources :deal_contents, path: 'proposition'
+      resources :deal_pms, path: 'message-privé'
     end
   end
-  resources :deals, only: %i[index], path: 'Troc'
+  resources :deals, only: %i[index], path: 'troc'
 
-  resources :discs, path: 'Disques'
+  resources :discs, path: 'disques'
   resources :genres
   resources :formats
   resources :articles
-  resources :messages, only: %i[new create], path: 'Contactez-nous'
+  resources :messages, only: %i[new create], path: 'contactez-nous'
 end
