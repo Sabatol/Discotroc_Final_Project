8bbdeb8579362981f6373d134784e6892980f332
piwiit <bouillaux@hotmail.fr>
2020-12-11 16:13:02 +0100

    update url mailler / indent all view

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ApplicationMailer
   def send_message_new_user(user)
     @user = user
-    @url = 'https://discotroc-dev.herokuapp.com'
+    @url = 'https://discotroc.herokuapp.com/'
     mail(to: @user.email, subject: 'Vous vous êtes inscrit sur Discotroc !')
   end
 end
