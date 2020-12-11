2eb9ce23ea3b06d52d532b2c6356ca498823f2f2
kill84 <killian.kos@gmail.com>
2020-12-11 16:55:56 +0100

    change mailer URLs for Heroku prod

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ApplicationMailer
   def send_message_new_user(user)
     @user = user
-    @url = 'https://discotroc-dev.herokuapp.com'
-    mail(to: @user.email, subject: 'Vous vous êtes inscrit sur Discotroc !')
+    @url = 'https://discotroc.herokuapp.com'
+    mail(to: @user.email, subject: 'Vous vous êtes inscrit(e) sur Discotroc !')
   end
 end
