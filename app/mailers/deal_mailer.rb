a42b1861ffcbeedc75b92fc20c6dc9c41f0df191
kill84 <killian.kos@gmail.com>
2020-12-13 10:58:48 +0100

    patch redirect URL

--- app/mailers/deal_mailer.rb
+++ app/mailers/deal_mailer.rb
@@ -2,14 +2,14 @@ class DealMailer < ApplicationMailer
   def send_message_new_deal_receiver(deal)
     @new_deal = deal
     @receiver = User.find(@new_deal.receiver_id)
-    @url = "https://discotroc.herokuapp.com/bibliothèques/#{@receiver_library}/Echange/#{@new_deal.id}"
+    @url = "https://discotroc.herokuapp.com/utilisateur/#{@new_deal.receiver.id}"
     mail(to: @receiver.email, subject: 'Nouveau troc')
   end
 
   def send_message_new_deal_sender(deal)
     @new_deal = deal
     @sender = User.find(@new_deal.sender_id)
-    @url = "https://discotroc.herokuapp.com/bibliothèques/#{@receiver_library}/Echange/#{@new_deal.id}"
+    @url = "https://discotroc.herokuapp.com/utilisateur/#{@new_deal.sender.id}"
     mail(to: @sender.email, subject: 'Nouveau troc')
   end
 end
